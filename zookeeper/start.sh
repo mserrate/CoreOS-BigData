ec2726ec28421b38679aa2e6926eb3decef2a7d6
kayrus <anton@endocode.com>
2015-03-13 12:40:21 +0100

    Updated zookeeper image and fleet files

--- zookeeper/start.sh
+++ zookeeper/start.sh
@@ -1,5 +1,10 @@
-#!/bin/sh
+#!/bin/bash
 
-printf "${ZK_SERVER_NUMBER}" > /var/lib/zookeeper/myid
-printf "Starting Zookeeper server $(cat /var/lib/zookeeper/myid)\n"
+if [[ -z "${ZK_ID}" || -z "${ZK_SERVERS}" ]]; then
+	echo "Please set ZK_ID and ZK_SERVERS environment variables first."
+	exit 1
+fi
+
+echo "${ZK_SERVERS}" | tr ' ' '\n' | tee -a /etc/zookeeper/conf/zoo.cfg
+echo "${ZK_ID}" | tee /var/lib/zookeeper/myid
 /usr/share/zookeeper/bin/zkServer.sh start-foreground
