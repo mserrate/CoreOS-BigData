13d76658fd24a2ece51200b6884347c0bdb224b8
mserrate <m.serrate@gmail.com>
2016-01-22 16:35:32 +0100

    Use default directories and mount on them

--- zookeeper/start.sh
+++ zookeeper/start.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
 
-if [[ -z "${ZK_ID}" || -z "${ZK_SERVERS}" || -z "${MEDIA_STORAGE}" ]]; then
-	echo "Please set ZK_ID, ZK_SERVERS & MEDIA_STORAGE environment variables first."
+if [[ -z "${ZK_ID}" || -z "${ZK_SERVERS}" ]]; then
+	echo "Please set ZK_ID & ZK_SERVERS environment variables first."
 	exit 1
 fi
 
 echo "${ZK_SERVERS}" | tr ' ' '\n' | tee -a /etc/zookeeper/conf/zoo.cfg
-sed -i -e "s/^dataDir.*/dataDir=\\$MEDIA_STORAGE/g" /etc/zookeeper/conf/zoo.cfg
-echo "${ZK_ID}" | tee $MEDIA_STORAGE/myid
+echo "${ZK_ID}" | tee /var/lib/zookeeper/myid
 /usr/share/zookeeper/bin/zkServer.sh start-foreground
